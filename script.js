7a549d885eb29a8535140e0683b8c2978dc92053
Schvea <sveaholm03@gmail.com>
2023-10-20 17:33:34 +0200

    fler trianglar

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ console.log(triangleOne);
 const baseTwo = 16.5;
 const heightTwo = 20.3;
 
-const triangleTwo = (baseTwo * heightTwo) / 2
-console.log(triangleTwo)
+const triangleTwo = (baseTwo * heightTwo) / 2;
+console.log(triangleTwo);
 
 console.log(Math.max(triangleOne, triangleTwo));
 
@@ -22,4 +22,12 @@ const heightThree = 16.5;
 
 const triangleThree = (baseThree * heightThree) /2;
 
-console.log(triangleThree)
+console.log(triangleThree == triangleTwo);
+
+//triangleFive: base = 7,8cm, height = 5,6cm
+
+//triangleSix: base = 9,3cm, height = 12,4cm
+
+const baseFour = 7.8;
+const height
+
